089753eb30ea89d1a1d18aab202653ec7e7b14ce
Gytė <Ne gyteb5@gmail.com>
2021-04-21 23:24:22 +0300

    Added button

--- app/src/main/java/com/example/fintechlab1/MainActivity.java
+++ app/src/main/java/com/example/fintechlab1/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.fintechlab1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textViewMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.textViewMain = findViewById(R.id.textViewMain);
+    }
+
+    public void OnClickHere(View view) {
+        this.textViewMain.setText("Text was changed!");
     }
 }
