53721e67c76c9a3e9dbe12fbcefd7f161ca8a50e
Gytė <Ne gyteb5@gmail.com>
2021-04-22 21:46:09 +0300

    Revert "Added button"
    This reverts commit 089753eb

--- app/src/main/java/com/example/fintechlab1/MainActivity.java
+++ app/src/main/java/com/example/fintechlab1/MainActivity.java
@@ -4,24 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
-
-    TextView textViewMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        this.textViewMain = findViewById(R.id.textViewMain);
-    }
-
-    public void OnClickHere(View view) {
-        this.textViewMain.setText("Text was changed!");
     }
 
 
